18de3055095c563663d614ad288f05ab74d7168f
samuele794 <samuelebruschi794@gmail.com>
2024-10-26 16:05:29 +0200

    WIP Presentation

--- src/main/kotlin/presentation/WhatIsIt.kt
+++ src/main/kotlin/presentation/WhatIsIt.kt
@@ -382,8 +382,8 @@ fun Presentation.WhatIsIt() {
                     unsafe {
                         raw(
                             """
-                            <lottie-player src="./mascot.json"  speed="1" style="width: 300px; height: 300px;" loop="" autoplay=""></lottie-player>
-                        """.trimIndent()
+                            <lottie-player src="mascot.json"  speed="1" style="width: 300px; height: 300px;" loop="" autoplay=""></lottie-player>
+                            """.trimIndent()
                         )
                     }
                 }
